7b910efcb50b9e8a3bfe18d7b541f48129f087c7
Hugo Mota <hugo.txt@gmail.com>
2017-05-18 20:26:07 -0300

    Add function to perform crossover operation on individuals

---
+++ crossover.go
@@ -0,0 +1,31 @@
+package main
+
+import "math/rand"
+
+func Crossover(parents [2]Individual, probability float64) [2]Individual {
+    if rand.Float64() > probability {
+        return parents
+    }
+    size := len(parents[0].vector)
+    offspring := makeEmptyIndividuals(size)
+    point := rand.Intn(size)
+    for i := 0; i < size; i++ {
+        if i < point {
+            offspring[0].vector[i] = parents[0].vector[i]
+            offspring[1].vector[i] = parents[1].vector[i]
+        } else {
+            offspring[0].vector[i] = parents[1].vector[i]
+            offspring[1].vector[i] = parents[0].vector[i]
+        }
+    }
+    copy(offspring[0].vector, parents[0].vector[:point])
+    return offspring
+}
+
+func makeEmptyIndividuals(size int) [2]Individual {
+    a := Individual{}
+    b := Individual{}
+    a.vector = make([]int, size)
+    b.vector = make([]int, size)
+    return [2]Individual{a, b}
+}
